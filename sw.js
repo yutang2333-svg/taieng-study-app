77a6a0b514a9acf67d4108b5ddd02adb05dc2553
yutang2333-svg <yutang2333@gmail.com>
2026-08-20 00:37:51 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taieng-coach-v4';
+const CACHE = 'taieng-coach-v5';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
